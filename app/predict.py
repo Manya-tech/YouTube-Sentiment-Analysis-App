98630a1adfb49d7b6b0fe55b9168f7492e1a85e9
Manya-tech <mgmanya2311@gmail.com>
2024-02-01 10:32:11 +0530

    fixed: import errors

--- app/predict.py
+++ app/predict.py
@@ -1,7 +1,7 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import pad_sequences
+from tensorflow import keras
+from keras.models import load_model
+from keras_preprocessing.sequence import pad_sequences
 import pickle
 
 VOCAB_SIZE = 10000
@@ -19,7 +19,7 @@ with open("tokenizer.pickle","rb") as handle:
 def encode_texts(text_list):
     encoded_texts = []
     for text in text_list:
-        tokens = tf.keras.preprocessing.text.text_to_word_sequence(text)
+        tokens = keras.preprocessing.text.text_to_word_sequence(text)
         tokens = [tokenizer.word_index[word] if word in tokenizer.word_index else 0 for word in tokens]
         encode_texts.append(tokens)
 
--- training.py
+++ training.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Embedding, Dense, GlobalAveragePooling1D
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow import keras
+from keras_preprocessing.sequence import pad_sequences
+from keras.models import Sequential, load_model
+from keras.layers import Embedding, Dense, GlobalAveragePooling1D
+from keras.preprocessing.text import Tokenizer
 import pickle
 # import warnings
 # warnings.filterwarnings('ignore')
