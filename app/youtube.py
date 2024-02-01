947a18b91adce01a4ca3948616be2b4313413ed2
Manya-tech <mgmanya2311@gmail.com>
2024-02-01 13:29:45 +0530

    edit: fixed errors

--- app/youtube.py
+++ app/youtube.py
@@ -1,7 +1,7 @@
 import os
 import googleapiclient.discovery
 import googleapiclient.errors
-import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,16 +25,16 @@ def get_comments(youtube, **kwargs):
 
     return comments
 
-def main(video_id, api_key):
+def main(video_id):
     # Disable OAuthlib's HTTPs verification when running locally.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     youtube = googleapiclient.discovery.build(
-        "youtube", "v3", developerKey=api_key)
+        "youtube", "v3", developerKey = api_key)
 
     comments = get_comments(youtube, part="snippet", videoId=video_id, textFormat="plainText")
     return comments
 
 
 def get_video_comments(video_id):
-    return main(video_id, api_key)
+    return main(video_id)
