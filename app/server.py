466bf5cf7b8520b37b0f4ae1768eb4ce91446001
Manya-tech <mgmanya2311@gmail.com>
2024-02-01 10:40:38 +0530

    add: function to generate summary of sentiment analysis

--- app/server.py
+++ app/server.py
@@ -2,3 +2,28 @@ from flask import Flask, request, render_template
 from predict import predict_sentiment
 from youtube import get_video_comments
 from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+def get_video(video_id):
+    if not video_id:
+        return {"error" : "video_id is required"}
+    
+    comments = get_video_comments(video_id)
+    predictions  = predict_sentiment(comments)
+
+    positive = predictions.count("Positive")
+    negative = predictions.count("Negative")
+
+    summary = {
+        "positive" : positive,
+        "negative" : negative,
+        "num_comments" : len(comments),
+        "rating" : (positive/len(comments))*100
+    }
+
+    return {"predictions" : predictions, "comments" : comments, "summary" : summary}
+
+
